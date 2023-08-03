f6d17efe5aa18ed4cff7e112244ad91abf2c47f2
Frédéric Perron <frederic.perr@gmail.com>
2023-08-03 02:43:51 -0400

    feat: plugins and analyzer integration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
+import env from "./env.ts";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+export default withPlugins(
+  [[withBundleAnalyzer({ enabled: env.BUNDLE_ANALYZER_ENABLED })]],
+  { nextConfig },
+);
